82507115e26dff23e863a2ae95f2a388fc6a44a8
saju madhavan <sajuptpm@gmail.com>
2016-04-27 21:27:40 +0000

    jnt 140 60 base.py

--- jnt_60.py
+++ jnt_60.py
@@ -1,80 +1,43 @@
-import client
+#!/usr/bin/env python
+
+from common import client
+from common import vpc_manager
+from common import subnet_manager
+from common import address_manager
 import unittest
 import netaddr
-
-VPC_QUOTA = 11
-SUBNET_QUOTA = 30
-ADDRESS_QUOTA = 10
+import settings
 
 class VpcQuotaCheck(unittest.TestCase):
     def setUp(self):
         self.jclient = client.Client()
-
-    def get_all_vpc_ids(self):
-        vpc_ids = []
-        res = self.jclient.vpc.describe_vpcs()
-        try:
-            vpcs = res['DescribeVpcsResponse']['vpcSet']['item']
-        except KeyError as ex:
-            pass
-        if isinstance(vpcs, list):
-            return [vpc['vpcId'] for vpc in vpcs]        
-        elif isinstance(vpcs, dict):
-            return [vpcs['vpcId']]
-        return vpc_ids
-
-    def clear_all_vpcs(self):
-        vpc_ids = self.get_all_vpc_ids()
-        print "......Cleaning VPCs: ", len(vpc_ids)
-        for vpc_id in vpc_ids:
-            self.jclient.vpc.delete_vpc(vpc_id=vpc_id)
+	self.vpc_manager = vpc_manager.VpcManager(self.jclient)
 
     def test_vpc_quota(self):
-	for x in range(VPC_QUOTA):
-            res = self.jclient.vpc.create_vpc(cidr_block='10.0.0.0/16')
+	cidr_block = '11.0.0.0/16'
+        net = netaddr.IPNetwork(cidr_block)
+        for vpc_cidr in list(net.subnet(28))[:settings.VPC_QUOTA+1]:
+            res = self.jclient.vpc.create_vpc(cidr_block=str(vpc_cidr))
             if res.get("Response"):
                 self.assertEqual(res['Response']['Errors']['Error']['Code'],  "VpcLimitExceeded")
                 return
         self.fail("Could not find expected exception VpcLimitExceeded")
 
     def tearDown(self):
-        self.clear_all_vpcs()
+        self.vpc_manager.delete_all_vpcs()
 
 class SubnetQuotaCheck(unittest.TestCase):
     def setUp(self):
         self.jclient = client.Client()
+	self.vpc_manager = vpc_manager.VpcManager(self.jclient)
+        self.subnet_manager = subnet_manager.SubnetManager(self.jclient)
         self.vpc_id = None
 
-    def create_vpc(self, cidr_block):
-        res = self.jclient.vpc.create_vpc(cidr_block=cidr_block)
-        vpc_id = res['CreateVpcResponse']['vpc']['vpcId']
-        return vpc_id
-
-    def get_all_subnet_ids(self):
-        subnet_ids = []
-        res = self.jclient.vpc.describe_subnets()
-        try:
-            subnets = res['DescribeSubnetsResponse']['subnetSet']['item']
-        except KeyError as ex:
-            pass
-        if isinstance(subnets, list):
-            return [subnet['subnetId'] for subnet in subnets if subnet['vpcId']==self.vpc_id]
-        elif isinstance(subnets, dict):
-            if subnets['vpcId'] == self.vpc_id:
-                return [subnets['subnetId']]
-        return subnet_ids
-
-    def clear_all_subnets(self):
-        subnet_ids = self.get_all_subnet_ids()
-        print "......Cleaning Subnets: ", len(subnet_ids)
-        for subnet_id in subnet_ids:
-            self.jclient.vpc.delete_subnet(subnet_id=subnet_id)
-
     def test_subnet_quota(self):
-        cidr_block = '10.0.0.0/16'
-        self.vpc_id = self.create_vpc(cidr_block)
+        cidr_block = '12.0.0.0/16'
+        self.vpc_id = self.vpc_manager.create_vpc(cidr_block)
         net = netaddr.IPNetwork(cidr_block)
-        for subnet_cidr in list(net.subnet(28))[:SUBNET_QUOTA]:
+        for subnet_cidr in list(net.subnet(28))[:settings.SUBNET_QUOTA+1]:
             res = self.jclient.vpc.create_subnet(vpc_id=self.vpc_id, cidr_block=str(subnet_cidr))
             if res.get("Response"):
                 self.assertEqual(res['Response']['Errors']['Error']['Code'],  "SubnetLimitExceeded")
@@ -82,33 +45,15 @@ class SubnetQuotaCheck(unittest.TestCase):
         self.fail("Could not find expected exception SubnetLimitExceeded")
 
     def tearDown(self):
-        self.clear_all_subnets()
+        self.subnet_manager.delete_all_subnets(self.vpc_id)
 
 class AddressQuotaCheck(unittest.TestCase):
     def setUp(self):
         self.jclient = client.Client()
+        self.address_manager = address_manager.AddressManager(self.jclient)
 
-    def get_all_address_ids(self):
-        address_ids = []
-        res = self.jclient.vpc.describe_addresses()
-        try:
-            addresses = res['DescribeAddressesResponse']['addressesSet']['item']
-        except KeyError as ex:
-            pass
-        if isinstance(addresses, list):
-            return [address['allocationId'] for address in addresses]
-        elif isinstance(addresses, dict):
-            return [addresses['allocationId']]
-        return address_ids
-
-    def clear_all_addresses(self):
-        address_ids = self.get_all_address_ids()
-        print "......Cleaning Addresses: ", len(address_ids)
-        for address_id in address_ids:
-            self.jclient.vpc.release_address(allocation_id=address_id)
-           
     def test_address_quota(self):
-        for x in range(ADDRESS_QUOTA):
+        for x in range(settings.ADDRESS_QUOTA+1):
             res = self.jclient.vpc.allocate_address(domain='vpc')
             if res.get("Response"):
 		self.assertEqual(res['Response']['Errors']['Error']['Code'],  "AddressLimitExceeded")
@@ -116,7 +61,7 @@ class AddressQuotaCheck(unittest.TestCase):
         self.fail("Could not find expected exception AddressLimitExceeded")
  
     def tearDown(self):
-        self.clear_all_addresses()
+        self.address_manager.delete_all_addresses()
 
 
 if __name__ == '__main__':
